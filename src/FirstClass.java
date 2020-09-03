222750d35e608791c848b28cf12d05bec2f96496
MacKenna Morris <23morris@da.org>
2020-09-03 10:27:32 -0400

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,8 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        String name = "MacKenna";
+        System.out.println("Dark theme is for the cool kids");
+        System.out.println("Written by: " + name);
+    }
+
+}
